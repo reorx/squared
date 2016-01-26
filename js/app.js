fc862a81c3ed38cf042e5b38497f03f0cab5608f
reorx <novoreorx@gmail.com>
2016-01-26 20:08:30 +0800

    playlist render

--- js/app.js
+++ js/app.js
@@ -8,12 +8,8 @@
             squared_box_dom = $('.squared-box')[0];
         squared_box.height(squared_box.width());
 
-        // Generate 9 grids
+        // use 9 grids
         squared_box.addClass('-grids-9');
-        for (var i = 0; i < 4; i++) {
-            var grid = $('<div>').addClass('grid-item').html(i);
-            squared_box.append(grid);
-        }
         squared_box.on('click',  '.close', function(e) {
             $(e.target).closest('.grid-item').remove();
         });
@@ -26,41 +22,44 @@
             filter: '.close',
         });
 
-        // Album list sortable
-        var album_list = $('.album-list');
-        album_list.find('li').each(function() {
-            Sortable.create(this, {
-                group: {
-                    name: 'default',
-                    pull: 'clone',
-                    put: false,
-                },
-                onMove: function (e) {
-                    if (e.to === squared_box_dom) {
-                        if (squared_box_dom.childNodes.length >= 9) {
-                            console.log('full');
-                            return false;
-                        }
-                    }
-                },
-                draggable: '.grid-item',
-                animation: 150,
-            });
-        });
-
 
         var playlist_template = Handlebars.compile($('#playlist-template').html());
-            renderPlaylist = function(json) {
-                var playlist = $(playlist_template(json));
 
-                // Bind sortable
-            };
+        var renderPlaylist = function(json) {
+            var $playlist = $('.play-list'),
+                rendered = $(playlist_template({playlist: json}));
+
+            $playlist.html(rendered);
+            // playlist sortable
+            $playlist.find('li').each(function() {
+                Sortable.create(this, {
+                    group: {
+                        name: 'default',
+                        pull: 'clone',
+                        put: false,
+                    },
+                    onMove: function (e) {
+                        if (e.to === squared_box_dom) {
+                            if (squared_box_dom.childNodes.length >= 9) {
+                                console.log('full');
+                                return false;
+                            }
+                        }
+                    },
+                    draggable: '.grid-item',
+                    animation: 150,
+                });
+            });
+
+            // Bind sortable
+        };
 
 
         // Bind url-box
         var url_box = $('.url-box');
-        url_box.on('change', function() {
-            var url = $(this).val();
+        url_box.find('input').on('change', function() {
+            console.log('changed', this.value);
+            var url = this.value;
             if (!url)
                 return;
 
